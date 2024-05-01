8031bb1ec119200938a7f77058edb146e96f8b71
JoseDCaballero <22090601@alumno.utmetropolitana.edu.mx>
2024-04-30 20:42:21 -0600

    se subio

--- src/router.js
+++ src/router.js
@@ -63,8 +63,8 @@ const routes = [
 
 const router = createRouter({
   /*history: createWebHistory(import.meta.env.BASE_URL)*/
-  history: createWebHistory(),
-  routes
+  history: createWebHistory('#'),
+  routes,
 })
 
 router.afterEach((to) => {
